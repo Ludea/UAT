a8598e45b5d46fdd701abefadb744371370dcf64
Ludea <ludovicw35@hotmail.com>
2022-04-22 09:27:37 +0200

    Update AssemblyInfo.cs

--- DotNetZip.Semverd/src/ZipReduced/Properties/AssemblyInfo.cs
+++ DotNetZip.Semverd/src/ZipReduced/Properties/AssemblyInfo.cs
@@ -7,8 +7,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Ionic's Zip Library (Reduced)")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
